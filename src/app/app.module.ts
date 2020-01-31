e449d2e6e8ca289be33abe22118a2f87bd36c7a2
Mitchell Yuen <35192626+CodeNameNinja@users.noreply.github.com>
2020-01-31 12:09:43 +0200

    added Angular Material Design and added Styling to Posts Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {MatInputModule, MatCardModule, MatButtonModule} from '@angular/material'
+
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/posts-create.component';
 
@@ -11,7 +14,11 @@ import { PostCreateComponent } from './posts/post-create/posts-create.component'
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
