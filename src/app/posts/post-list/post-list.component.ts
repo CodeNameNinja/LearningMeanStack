c20cf51118b051003a3f2ae5c07f91ba2f762a9f
Mitchell Yuen <35192626+CodeNameNinja@users.noreply.github.com>
2020-01-31 14:20:00 +0200

    added Post Model

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-
+import { Post } from '../post.model';
 @Component({
 selector:'app-post-list',
 templateUrl:'post-list.component.html',
@@ -12,5 +12,5 @@ export class PostListComponent{
     //     {title:'Second Post',content:'This is the Second Post'},
     //     {title:'Third Post',content:'This is the Third Post'}
     // ];
-    @Input() posts=[];
+    @Input() posts: Post[] = [];
 }
